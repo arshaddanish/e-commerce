7426719b4a1c3d1481bd7b86782c445782a938ef
Arshad Danish <arshaddanishthana@gmail.com>
2021-11-18 20:27:53 +0530

    dynamically route to collection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
     component: ResetPasswordComponent
   },
   {
-    path: "collection",
+    path: "collection/:name",
     component: ProductsComponent
   }
 ];
--- src/app/components/collection/products/products.component.ts
+++ src/app/components/collection/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TRENDING } from 'src/app/data/TRENDING';
 import { trendings } from 'src/app/data/trendings';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -9,10 +10,12 @@ import { trendings } from 'src/app/data/trendings';
 })
 export class ProductsComponent implements OnInit {
   products: TRENDING[] = trendings;
+  collection: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.collection = this.route.snapshot.params['name'];
   }
 
 }
