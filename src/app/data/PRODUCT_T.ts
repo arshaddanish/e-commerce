5120dabfca3eb726b3ad3ca14dd3c7095400dcd5
Arshad Danish <arshaddanishthana@gmail.com>
2021-11-20 13:09:48 +0530

    add product data

---
+++ src/app/data/PRODUCT_T.ts
@@ -0,0 +1,9 @@
+export interface PRODUCT_T {
+  id?: number,
+  item: string,
+  cost: number,
+  img1: string,
+  img2: string,
+  img3: string,
+  img4: string
+}
---
+++ src/app/data/product.ts
@@ -0,0 +1,11 @@
+import { PRODUCT_T } from "./PRODUCT_T";
+
+export const product: PRODUCT_T = {
+  id: 1,
+  item: 'Light Denim Jacket',
+  cost: 1000,
+  img1: "../../../../assets/images/product/1.jpg",
+  img2: "../../../../assets/images/product/2.jpg",
+  img3: "../../../../assets/images/product/3.jpg",
+  img4: "../../../../assets/images/product/4.jpg",
+}
