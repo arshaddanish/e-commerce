586a4ff50412854b5d772e7d60e5457e80f83fbf
Arshad Danish <arshaddanishthana@gmail.com>
2021-11-22 23:11:17 +0530

    minor

--- src/app/data/product.ts
+++ src/app/data/product.ts
@@ -4,8 +4,8 @@ export const product: PRODUCT_T = {
   id: 1,
   item: 'Light Denim Jacket',
   cost: 1000,
-  img1: "../../../../assets/images/product/1.jpg",
-  img2: "../../../../assets/images/product/2.jpg",
+  img1: "../../../../assets/images/product/2.jpg",
+  img2: "../../../../assets/images/product/1.jpg",
   img3: "../../../../assets/images/product/3.jpg",
   img4: "../../../../assets/images/product/4.jpg",
 }
